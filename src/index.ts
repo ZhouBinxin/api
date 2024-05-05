d493206a2be4668f0b6800c6535bcbbd4bac5ae6
zhoubinxin <binxin.zhou@outlook.com>
2024-05-05 19:55:28 +0800

    完成基本框架搭建

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Environment } from "vitest";
+
+async function handleRequest (request: Request, env: Environment) {
+	const url = new URL(request.url);
+	const path = url.pathname;
+	const method = request.method;
+
+	const headers = {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "*",
+		"Access-Control-Allow-Headers": "*",
+		"Content-Type": "application/json",
+	}
+
+	if (method === "OPTIONS") {
+		return new Response(null, { headers });
+	}
+
+	const date = new Date();
+	const formatter = new Intl.DateTimeFormat('zh-CN', {
+		timeZone: 'Asia/Shanghai',
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+	const timestamp = formatter.format(date);
+
+	let data = {
+		timestamp: timestamp,
+	}
+
+	let status = 200;
+
+	if (path === "/") {
+		return new Response(JSON.stringify(data), { status, headers });
+	} else if (path === "/favicon.ico") {
+		return handleFavicon();
+	}
+}
+
+async function handleFavicon () {
+	const faviconUrl = 'https://blog.bxin.top/img/favicon.ico';
+	return fetch(faviconUrl);
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch (request: Request, env: Environment) {
+		return handleRequest(request, env);
 	},
 };
